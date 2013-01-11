b46ea6216f7bce6714fb09e0c5ce381e2302a0fa
Jose F. Romaniello <jfromaniello@gmail.com>
2013-01-11 09:27:10 -0300

    add support to download with liburl2 when ssl is enabled

--- package_decontrol.py
+++ package_decontrol.py
@@ -2,7 +2,10 @@ import sublime_plugin
 import subprocess
 import os
 import zipfile
+import sys
+import urllib2
 
+urllib2.install_opener(urllib2.build_opener(urllib2.ProxyHandler()))
 packages_path = os.path.join(os.getcwdu(), '../')
 
 
@@ -13,9 +16,20 @@ class PackageDecontrolCommand(sublime_plugin.WindowCommand):
     def on_done(self, name):
         print('Installing' + name)
         tempfile = name.replace('/', '-') + '.zip'
-        self.download_with_curl(name, tempfile)
+        self.download(name, tempfile)
         self.unzip(name, tempfile)
         os.remove(tempfile)
+
+    def download(self, name, tempfile):
+        if ('ssl' not in sys.modules and os.name != 'nt'):
+            self.download_with_curl(name, tempfile)
+        elif('ssl' in sys.modules):
+            self.download_native(name, tempfile)
+
+    def download_native(self, name, tempfile):
+        url = "https://github.com/%s/archive/master.zip" % name
+        request = urllib2.urlopen(url)
+        open(tempfile, 'wb').write(request.read())
 
     # TODO, create a version for windows..
     # I am using this because my python doesnt have ssl.
