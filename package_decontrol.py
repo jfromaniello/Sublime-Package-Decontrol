b31d18ba168e7492067936c9788ee2c28407c9bc
José F. Romaniello <jfromaniello@gmail.com>
2013-04-26 08:06:36 -0300

    add support for branchs

--- package_decontrol.py
+++ package_decontrol.py
@@ -16,49 +16,51 @@ class PackageDecontrolCommand(sublime_plugin.WindowCommand):
 
     def on_done(self, name):
         print('Installing package %s' % name)
+        
+        name, branch = (name.split("#") + ["master"])[:2]
+
         tempfile = name.replace('/', '-') + '.zip'
-        self.download(name, tempfile)
+        self.download(name, tempfile, branch)
         self.unzip(name, tempfile)
         os.remove(tempfile)
 
-    def download(self, name, tempfile):
+    def download(self, name, tempfile, branch = "master"):
         def exist(name):
             try:
                 return subprocess.call([name, '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
             except:
                 return False
 
+        url = "https://github.com/%s/archive/%s.zip" % (name, branch)
+        
         if('ssl' in sys.modules):
             print('installing with liburl')
-            self.download_native(name, tempfile)
+            self.download_native(url, tempfile)
         else:
             if(os.name == 'nt'):
                 print("can't download in Windows without ssl")
             else:
                 if(exist('curl')):
                     print('installing with curl')
-                    self.download_with_curl(name, tempfile)
+                    self.download_with_curl(url, tempfile)
                 elif(exist('wget')):
                     print('installing with wget')
-                    self.download_with_curl(name, tempfile)
+                    self.download_with_curl(url, tempfile)
                 else:
                     print('you need to install curl or wget in order to install packages')
 
-    def download_native(self, name, tempfile):
-        url = "https://github.com/%s/archive/master.zip" % name
+    def download_native(self, url, tempfile):
         request = urllib.request.urlopen(url)
         open(tempfile, 'wb').write(request.read())
 
-    def download_with_curl(self, name, tempfile):
-        url = "https://github.com/%s/archive/master.zip" % name
+    def download_with_curl(self, url, tempfile):
         command = ['curl', '-L', url]
 
         f = open(tempfile, 'w')
         proc = subprocess.Popen(command, stdout=f, stderr=subprocess.PIPE)
         proc.wait()
 
-    def download_with_wget(self, name, tempfile):
-        url = "https://github.com/%s/archive/master.zip" % name
+    def download_with_wget(self, url, tempfile):
         command = ['wget', '-O', tempfile, url]
 
         proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -80,7 +82,7 @@ class PackageDecontrolCommand(sublime_plugin.WindowCommand):
             if filename == '.gitignore':
                 continue
 
-            fd = open(os.path.join(dirname, filename), "w")
+            fd = open(os.path.join(dirname, filename), "wb")
             fd.write(zf.read(name))
             fd.close()
 
